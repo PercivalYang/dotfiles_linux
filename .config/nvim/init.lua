8d25def2b5f33b572976fa85c05d27d1dbc8a116
PercivalYang <yzy1998719@163.com>
2022-12-01 18:06:32 +0800

    Init neovim with lua configuration

---
+++ .config/nvim/init.lua
@@ -0,0 +1,20 @@
+-- 基础设置
+require('basic')
+-- 快捷键映射
+require("keybindings")
+-- 插件设置
+require("plugins")
+-- 主题方案
+require("colorscheme")
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
+require("lsp.setup")
+require("lsp.cmp")
+require("lsp.ui")
+require("plugin-config.indent-blankline")
